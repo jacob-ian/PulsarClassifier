47b91b500030589aaeab11c365f8856f9c4819be
Jacob Ian Matthews <jacob@jacobian.com.au>
2020-05-21 20:54:42 +0800

    readme

---
+++ src/main/java/classifier/PulsarClassifier.java
@@ -0,0 +1,22 @@
+package classifier;
+
+/**
+ * This file is a part of PulsarClassifier.
+ * 
+ * It is a derivative software of the LOTAASClassifier created by Rob Lyon, located at https://github.com/scienceguyrob/LOTAASClassifier, produced under the GNU General Public License.
+ * 
+ * Author: Jacob Ian Matthews
+ * Version: 1.0
+ * Date: 20/05/2020
+ * Contact: jacob@jacobian.com.au
+ * 
+ */
+
+
+public class PulsarClassifier 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
